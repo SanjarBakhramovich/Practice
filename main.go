e100b5f1ffcf8c8174222cc1813da67a6b070cf5
Sanjar Usmonov <sanjarbakhramovich@gmail.com>
2024-10-30 17:57:34 +0500

    first task done

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var task string
+
+func POSTHandler(w http.ResponseWriter, r *http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var t struct {
+		Task string `json:"task"`
+	}
+	if err := decoder.Decode(&t); err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	task = t.Task
+	w.WriteHeader(http.StatusOK)
+}
+
+
+
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello", task)
+}
+
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", POSTHandler).Methods("POST")
+	http.ListenAndServe(":8080", router)
+}
